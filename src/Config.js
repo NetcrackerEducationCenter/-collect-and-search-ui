55d228017230d0087e1665779ef9346647aef9db
Андраник Вачаганович Григорян <53790991+MajorPayne36@users.noreply.github.com>
2021-04-21 23:00:03 +0300

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 export const config = {
-     url: "http://localhost:7071", //206.81.22.187
+     url: "http://206.81.22.187:7071", //206.81.22.187
      ws: 'ws://localhost:7071',
      UPDATE: 'update',
      DELETE: 'remove',
